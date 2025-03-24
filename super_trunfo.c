fd196f5e2d5da24f80a514967269a73e825f57b1
rafael011997 <rafaelpmiller27@gmail.com>
2025-03-24 00:47:10 +0000

    polido

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,83 +1,62 @@
 #include <stdio.h>
 
 int main(){
+    char estado[10],estado2[10];
+    char codigodecarta[50], codigodecarta2[50];
+    char nomedacidade[50], nomedacidade2[50];
+    int populacao, populacao2;
+    float areaemkm, areaemkm2;
+    float pib, pib2;
+    int numerodeposntosturistico, numerodeposntosturistico2;
+
+    printf("***JOGO SUPER TRUNFO***\n");
+    printf("entre com a Carta 01\n");
+    printf("Entre com Estado (A-H):\n");
+    scanf("%s", &estado);
+    printf("Codigo da Carta(ex: A01, B03):\n");
+    scanf("%s", &codigodecarta);
+    printf("Nome da Cidade:\n");
+    scanf("%s", &nomedacidade);
+    printf("População:\n");
+    scanf("%d", &populacao);
+    printf("Área (em km²):\n");
+    scanf("%f", &areaemkm);
+    printf("PIB:\n");
+    scanf("%f", &pib);
+    printf("Número de Pontos Turísticos:\n");
+    scanf("%d", &numerodeposntosturistico);
     
-    printf("***SUPER TRUNFO***\n");
-
-    char ncarta = 1;                                               
-    char estado = 'A';                                   
-    char carta[50] = "A01";                               
-    char cidade[20] = "Niterói";                         
-    unsigned long int populacao = 481749;                                  
-    float area = 133.757;  //Area da cidade                               
-    float pib = 128.333;                                 
-    int turismo = 8; 
-    float media = populacao / area;  ///densidadde populacional (float)
-    float media1 = populacao / pib ;  ///PIB per capital (float)                               
-    float superpoder = populacao + area + pib + turismo +  media + media1;
+    printf("entre com a Carta 02\n");
+    printf("Entre com Estado (A-H):\n");
+    scanf("%s", &estado2);
+    printf("Código da Carta(ex: A01, B03):\n");
+    scanf("%s", &codigodecarta2);
+    printf("Nome da Cidade:\n");
+    scanf("%s", &nomedacidade2);
+    printf("População:\n");
+    scanf("%d", &populacao2);
+    printf("Área (em km²):\n");
+    scanf("%f", &areaemkm2);
+    printf("PIB:\n");
+    scanf("%f", &pib2);
+    printf("Número de Pontos Turísticos:\n");
+    scanf("%d", &numerodeposntosturistico2);
 
     printf("\n");
-    printf("Carta: %i\n", ncarta);
-    printf("Estado: %c\nCódigo: %s\n", estado, carta);
-    printf("Nome da Cidade: %s\n", cidade);
-    printf("Populacão: %i\nÁrea: %.3fKm²\n", populacao, area);
-    printf("PIB: R$%.3f bilhões de reais\nNúmero de Pontos Turístico: %i\n", pib, turismo);
-    printf("Densidade Populacional: %.2f hab/km²\n", media);
-    printf("PIB per Capita: %.2f Reais\n", media1);
-    printf("super Poder %.2f\n", superpoder);
+
+    printf("Carta 1:\n");
+    printf("Estado: %s \nCódigo da Carta: %s\n", estado, codigodecarta);
+    printf("Nome da Cidade: %s \nPopulação: %d\n", nomedacidade, populacao);
+    printf("Área em km²: %f\nPIB: %f\n",areaemkm, pib); 
+    printf("Número de Pontos Turísticos: %d\n", numerodeposntosturistico);
     
     printf("\n");
+
+    printf("Carta 2:\n");
+    printf("Estado: %s \nCódigo da Carta: %s\n", estado2, codigodecarta2);
+    printf("Nome da Cidade: %s \nPopulação: %d\n", nomedacidade2, populacao2);
+    printf("Área em km²: %f\nPIB: %f\n",areaemkm2, pib2); 
+    printf("Número de Pontos Turísticos: %d\n", numerodeposntosturistico2);
     
-    char Ncarta = 2;
-    char Estado = 'B';
-    char Carta[40] = "B02";
-    char Cidade[50] = "Macaé";
-    unsigned long int Populacao = 246391;
-    float Area = 1.216;   //Area da cidade
-    float Pib = 66.684; 
-    int Turismo = 10;
-    float Media = Populacao / Area;  ///densidadde populacional (float)
-    float Media1 = Populacao / Pib ;  ///PIB per capital (float)
-    float Superpoder = Populacao + Area + Pib + Turismo +  Media + Media1;
-
-    printf("\n");
-    printf("Carta: %i\n", Ncarta);
-    printf("Estado: %c\nCódigo: %s\n", Estado, Carta);
-    printf("Nome da Cidade: %s\n", Cidade);
-    printf("Populacão: %i\nÁrea: %.3fKm²\n", Populacao, Area);
-    printf("PIB: R$%.3f bilhões de reais\nNúmero de Pontos Turístico: %i\n", Pib, Turismo);
-    printf("Densidade Populacional: %.2f hab/km²\n", Media);
-    printf("PIB per Capita: %.2f Reais\n", Media1);
-    printf("super Poder %.2f\n", Superpoder);
-
-    printf("\n");
-    printf("***Comparação de Cartas***");
-    printf("\n");
-
-   // char resultado1 = populacao > Populacao;
-   // char resultado2 = area > Area;
-   // char resultado3 = pib > Pib;
-  //  char resultado4 = turismo > Turismo;
-  //  char resultado5 = media > Media;
- //   char resultado6 = media1 > Media1;
-  //  char resultado7 = superpoder > Superpoder;
-
-
-   // printf("Populacao: Carta 1 Venceu (%u)\n", resultado1);
-   // printf("Area: carta 1 Venceu (%u)\n", resultado2);
-   // printf("PIB: Carta 1 Venceu (%u)\n", resultado3);
-   // printf("Ponto Turistico: Carta 2 Venceu(%u)\n", resultado4);
-   // printf("Densidade Populacional: Carta 2 Venceu  (%u)\n", resultado5);
-   // printf("PIB per capital: carta 1 Venceu(%u)\n", resultado6);
-   // printf("Super Poder: carta 1 Venceu(%u)\n", resultado7);
-
-   printf ("Populacao: carta %i venceu (%u)\n", (ncarta) , (populacao > Populacao));
-   printf ("Área: carta %i venceu (%u)\n", (ncarta) , (area > Area));
-   printf ("PIB: carta %i venceu (%u)\n", (ncarta) , (pib > Pib));
-   printf ("Ponto Turistico: carta %i venceu (%u)\n", (Ncarta) , (turismo > Turismo));
-   printf ("Densidade Populacional: carta %i venceu (%u)\n", (ncarta) , (media < Media));
-   printf ("PIB per capital: carta %i venceu (%u)\n", (ncarta) , (media1 > Media1));
-   printf ("Super Poder: carta %i venceu (%u)\n", (ncarta) , (superpoder > Superpoder));
-
-   return 0;
+    return 0;
 }
